50d4bf9f2f343dfe654203f05b1bd0cbe765c71d
sanchaa <alexanderagudelo90@gmail.com>
2024-06-07 00:37:04 -0500

    added selenium job

--- test/selenium/libraries/selenium_env_local.py
+++ test/selenium/libraries/selenium_env_local.py
@@ -6,6 +6,7 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver import ChromeOptions
 
 
 def createEnvironment():
@@ -24,10 +25,16 @@ def createEnvironment():
 def get_driver(browser_name: str):
 
     if browser_name == 'CHROME':
-        return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        options = ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        return webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
     elif browser_name == 'FIREFOX':
         return webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-    
+
     elif browser_name == 'EDGE':
         return webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
